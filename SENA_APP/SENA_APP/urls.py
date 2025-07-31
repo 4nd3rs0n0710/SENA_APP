5edf46376793e7a69f143567a1a1db2ed42b148a
Anderson Forero <4nd3rs0nf0r3r0@gmail.com>
2025-07-31 11:45:09 -0500

    Creación de la aplicación Programas

--- SENA_APP/SENA_APP/urls.py
+++ SENA_APP/SENA_APP/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('', main_views.index, name='inicio'),
     path('aprendices/', include('aprendices.urls')),
     path('instructores/', include('instructores.urls')),
+    path('', include('programas.urls')),
 ]
+
+# Personalización del pnel administrativo
+
+admin.site.site_header = "Panel Administrativo SENA"
+admin.site.site_title = "SENA APP"
+admin.site.site_title = "Gestión de Aprendices"
--- SENA_APP/programas/admin.py
+++ SENA_APP/programas/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Programa
 
-# Register your models here.
+@admin.register(Programa)
+class ProgramaFormacionAdmin(admin.ModelAdmin):
+    list_display = [
+        'codigo',
+        'nombre',
+        'nivel_formacion',
+        'modalidad',
+        'duracion_meses',
+        'duracion_horas',
+        'centro_formacion',
+        'regional',
+        'estado',
+        'fecha_creacion'
+    ]
+    
+    list_filter = ['modalidad', 'nivel_formacion', 'estado', 'regional']
+    
+    search_fields = [
+        'codigo',
+        'nombre',
+        'centro_formacion',
+        'regional',
+        'descripcion'
+    ]
+    
+    list_per_page = 25
+    
+    ordering = ['nombre', 'codigo']
